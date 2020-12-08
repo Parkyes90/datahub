83a09165da251f3fb1a672232a91f96b9c76598d
parkyes90 <parkyes90@gmail.com>
2020-12-08 13:44:52 +0900

    fix #2

--- results.py
+++ results.py
@@ -2,26 +2,27 @@ import csv
 import os
 import sys
 
-from bs4 import BeautifulSoup
 
-from common import DATA_DIR
+from common import DATA_DIR, write_csv
 
 csv.field_size_limit(sys.maxsize)
 
 if __name__ == "__main__":
     ret = []
-    columns = ["index", "year", "title", "category", "context"]
+    columns = ["year", "title", "category", "context"]
     result_path = os.path.join(DATA_DIR, "output", "results.csv")
     with open(result_path) as f:
         reader = csv.reader(f)
         for i, row in enumerate(reader):
-            if i < 17698:
-                continue
-            if i == 18000:
-                break
             if i == 0:
                 continue
+
             index, year, title, category, context = row
+            # index = index.replace('"', "")
+            # year = f"{year}"
+            # title = title.replace('"', "")
+            # category = category.replace('"', "")
+            context = context.replace(" ", " ").replace("​", " ")
             temp = {
                 "index": index,
                 "year": year,
@@ -29,8 +30,6 @@ if __name__ == "__main__":
                 "category": category,
                 "context": context,
             }
-            soup = BeautifulSoup(context, "html.parser")
-            print(soup.text)
             for column in columns:
                 if not temp[column].startswith('"'):
                     temp[column] = f'"{temp[column]}'
@@ -38,3 +37,6 @@ if __name__ == "__main__":
                     temp[column] = f'{temp[column]}"'
 
             ret.append(temp)
+
+    # print(ret[0])
+    write_csv(ret, "output", "results.csv")
